facb8700fc7eafec2ddf46fcc2d47e0bbd3a0f22
Jsunseri <jss97@pitt.edu>
2019-01-10 13:07:33 -0500

    get_num_frames just for .nc files for now (it asserts the input is that type), stale change to translate_rec

---
+++ get_num_frames.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import MDAnalysis
+import sys,os
+
+assert len(sys.argv[1:]) == 2, "Input: get_num_frames.py <topology> <traj>\n"
+assert os.path.splitext(sys.argv[1])[-1] == '.prmtop', "Arg1 should be *.prmtop\n"
+assert os.path.splitext(sys.argv[2])[-1] == '.nc', "Arg2 should be *.nc\n"
+topology = sys.argv[1]
+trajectory = sys.argv[2]
+
+frames = MDAnalysis.Universe(topology,trajectory)
+print (frames.trajectory.totaltime + frames.trajectory.dt) * 1e-3
--- translate_rec.py
+++ translate_rec.py
@@ -11,7 +11,10 @@ if __name__ == "__main__":
     "its origin corresponds to the location of the other's center")
     parser.add_argument('-m', '--mobile', required=True, help='Molecule to translate')
     parser.add_argument('-r', '--ref', required=True, help='Molecule to define origin')
-    parser.add_argument('-o', '--outname', default='', help='Optionally specify output filename')
+    parser.add_argument('-o', '--outname', default='', help='Optionally specify output' 
+    'filename prefix')
+    parser.add_argument('-b', '--move_both', default=False,
+            action='store_true', help='Translate both molecules')
     args = parser.parse_args()
 
     assert os.path.isfile(args.mobile), '%s does not exist' %args.mobile
@@ -23,4 +26,13 @@ if __name__ == "__main__":
     outname = args.outname
     if not outname:
         outname = os.path.splitext(args.mobile)[0] + '_translated.pdb'
+    else:
+        outname = outname + '_rec.pdb'
     mobile.writepdb(outname)
+    if args.move_both:
+        ref.set_origin(center)
+        if args.outname:
+            outname = args.outname + '_lig.pdb'
+        else:
+            outname = os.path.splitext(args.ref)[0] + '_translated.pdb'
+        ref.writepdb(outname)
